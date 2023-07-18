7d4213af4babb686733deadd470e95ec570c7a04
aliuzair999 <aliuzair999@gmail.com>
2023-07-18 18:18:58 +0500

    updating portfolio

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 import './Experience.css'
 
+import { themeContext } from '../../Context';
+import { useContext } from 'react';
 
 const Experience = () => {
 
+    const theme = useContext(themeContext)
+    const darkMode = theme.state.darkMode
 
     return (
         <div className="experience" id="Experience">
             <div className="achievement">
                 <div className="circle">8+</div>
-                <span>Years</span>
+                <span style={darkMode ? { color: "white" } : { color: "" }}>Years</span>
                 <span>Experience</span>
             </div>
 
             <div className="achievement">
                 <div className="circle">20+</div>
-                <span>Project</span>
+                <span style={darkMode ? { color: "white" } : { color: "" }}>Project</span>
                 <span>Completed</span>
             </div>
 
             <div className="achievement">
                 <div className="circle">5+</div>
-                <span>Companies</span>
+                <span style={darkMode ? { color: "white" } : { color: "" }}>Companies</span>
                 <span>Worked</span>
             </div>
 
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -3,6 +3,9 @@ import "./Portfolio.css"
 import { Swiper, SwiperSlide } from "swiper/react"
 import Olx from "../../img/olx.png";
 import Elo from "../../img/elo.png";
+import realEstate from '../../img/portfolio/realEstate.PNG'
+import LuckyDraw from '../../img/portfolio/luckyDraw.PNG'
+import chatApp from '../../img/portfolio/chatApp.PNG'
 import Food from "../../img/food.png";
 import Reume from "../../img/portfolio.png";
 import "swiper/css";
@@ -29,7 +32,7 @@ const Portfolio = () => {
 
             {/* HEADING */}
             <span style={darkMode ? { color: "white" } : { color: "" }} >Recent Project</span>
-            <span>Portfolio</span>
+            <span className="port-head">Portfolio</span>
 
             {/* SLIDER */}
 
@@ -40,25 +43,63 @@ const Portfolio = () => {
                 className="portfolio-slider"
             >
                 <SwiperSlide>                   
+                        <img src={realEstate} alt="realEstate" onClick={() => {
+                        window.open('https://uzair-real-estate.netlify.app/', '_blank');
+                    }}/>
+                    <span className="portfolio-text">
+                        Real Estate
+                    </span>
+                </SwiperSlide>
+                
+                <SwiperSlide>                   
                         <img src={Elo} alt="Elo" onClick={() => {
                         window.open('https://elo-app.netlify.app/', '_blank');
                     }}/>
+                     <span className="portfolio-text">
+                        Ecommerce App
+                    </span>
+                </SwiperSlide>
+                <SwiperSlide>                   
+                        <img src={LuckyDraw} alt="LuckyDraw" onClick={() => {
+                        window.open('https://uzair-luckydraw.netlify.app/', '_blank');
+                    }}/>
+                     <span className="portfolio-text">
+                        Luckydraw App
+                    </span>
+                </SwiperSlide>
+                <SwiperSlide>                   
+                        <img src={chatApp} alt="chatApp" onClick={() => {
+                        window.open('https://drive.google.com/file/d/1sONVI6pWA8FXlNmJVbptxCUZAHE5K3Nq/view?usp=sharing', '_blank');
+                    }}/>
+                     <span className="portfolio-text">
+                        Chat App
+                    </span>
                 </SwiperSlide>
                 <SwiperSlide className="swiper-slide">
                     <img src={Olx} alt="Olx" onClick={() => {
                         window.open('https://ad-posting-app.netlify.app/', '_blank');
                     }}/>
+                     <span className="portfolio-text">
+                        OLX
+                    </span>
                 </SwiperSlide>
                 <SwiperSlide>
                     <img src={Reume} alt="Portfolio" onClick={() => {
                         window.open('https://github.com/AliUzair999/portfolio', '_blank');
                     }}/>
+                     <span className="portfolio-text">
+                        Portfolio
+                    </span>
                 </SwiperSlide>
                 <SwiperSlide>
                     <img src={Food} alt="Food" onClick={() => {
                         window.open('https://restaurant-app-uzair.netlify.app/', '_blank');
                     }}/>
+                     <span className="portfolio-text">
+                        Food Ordering
+                    </span>
                 </SwiperSlide>
+                
 
             </Swiper>
         </div>
