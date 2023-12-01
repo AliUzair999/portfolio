196edb55ebef5bb7bdc0208d23aa4641f7b379c7
aliUzair999 <aliuzair999@gmail.com>
2023-12-01 23:58:36 +0500

    meta data updated

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
-import'./App.css' 
+import "./App.css";
 
 import Navbar from "./components/Navbar/Navbar";
-import Intro from './components/Intro/Intro';
-import Services from './components/Services/Services';
-import Experience from './components/Experience/Experience';
-import Work from './components/Work/Work';
-import Portfolio from './components/Portfolio/Portfolio';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
+import Intro from "./components/Intro/Intro";
+import Services from "./components/Services/Services";
+import Experience from "./components/Experience/Experience";
+import Work from "./components/Work/Work";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
-import { themeContext } from './Context';
-import { useContext } from 'react';
+import { themeContext } from "./Context";
+import { useContext } from "react";
+import { Helmet } from "react-helmet";
 
 function App() {
-
-  const theme = useContext(themeContext)
-  const darkMode = theme.state.darkMode
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
 
   return (
-    <div className="App" style={{
-      background: darkMode ? 'black' : '',
-      color: darkMode ? 'white' : ''
-    }}>
-        <Navbar />
-        <Intro />
-        <Services />
-        <Experience />
-        <Work />
-        <Portfolio />
-        <Contact />
-        <Footer />
-      
+    <div
+      className="App"
+      style={{
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
+      }}
+    >
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Ali Uzair Resume</title>
+        <link rel="canonical" href="https://resume-ali.netlify.app/" />
+        <meta
+          name="description"
+          content="I am a creative MERN stack developer blending innovation with technology. Expert in MongoDB, Express.js, React, and Node.js, ready to tackle web development challenges."
+        />
+      </Helmet>
+      ;
+      <Navbar />
+      <Intro />
+      <Services />
+      <Experience />
+      <Work />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </div>
   );
 }
